6350384270670015ae593b1cfd70c2b89094e6ab
Avinash Kumar Yadav <avinashyadav060298@gmail.com>
2022-07-12 16:22:22 +0530

    app js modified

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 
 var app= express();
 
+const path = require('path');
 
 var port = process.env.PORT || 3000;
 
-app.get('/',function(req,res){
+app.use(express.static(path.join(__dirname,'public')));
+
+/* app.get('/',function(req,res){
     res.send('Hello World from Azure');
 });
 
-/* app.get('/',(req,res) => res.send('Hello World')); */
+ app.get('/',(req,res) => res.send('Hello World')); */
+
+ app.get('/',(req,res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+ });
 
 app.listen(port,() => 
     console.log('Server is running..')
