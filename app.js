9bd1b2af9df71cb1286347ab9840c61b89954124
Avinash Kumar Yadav <avinashyadav060298@gmail.com>
2022-07-12 14:23:49 +0530

    added web config

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app= express();
 var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
-    res.send('Hello World');
+    res.send('Hello World from Azure');
 });
 
 /* app.get('/',(req,res) => res.send('Hello World')); */
