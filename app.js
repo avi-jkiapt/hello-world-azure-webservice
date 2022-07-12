c65e3b16887bc4fd2860cd17647830816b7920c1
Avinash Kumar Yadav <avinashyadav060298@gmail.com>
2022-07-12 10:42:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+var app= express();
+
+
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+/* app.get('/',(req,res) => res.send('Hello World')); */
+
+app.listen(port,() => 
+    console.log('Server is running..')
+);
